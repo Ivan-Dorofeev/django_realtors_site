51083ae9557155b38bfe61540dc506aec4f2fd2f
Ivan Dorofeev <axxel123@rambler.ru>
2022-08-12 08:49:55 +0300

    Убрал дуюлирование данных из моделей

--- property/admin.py
+++ property/admin.py
@@ -10,10 +10,9 @@ class OwnersInLine(admin.TabularInline):
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner']
+    search_fields = ['town', 'address']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber',
-                    'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'active']
     raw_id_fields = ['liked_by']
